3848df94ca73c1156d2795e9b8cd043de8601aa1
Felix Schmorell <felix.schmorell@dialog.ch>
2018-03-08 08:07:09 +0100

    hybrid app

--- src/ng2/app/app.module.ts
+++ src/ng2/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { UpgradeModule, downgradeComponent } from '@angular/upgrade/static';
 
 import { AppComponent } from './app.component';
+import { Ng2DemoComponent } from './ng2-demo/ng2-demo.component';
 
+declare var angular: any;
+
+angular.module('phonecatApp')
+  .directive(
+    'ng2Demo',
+    downgradeComponent({component: Ng2DemoComponent})
+  );
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Ng2DemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UpgradeModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap: [AppComponent],
+  entryComponents: [
+    Ng2DemoComponent // Don't forget this!!!
+  ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private upgrade: UpgradeModule) { }
+  ngDoBootstrap() {
+    this.upgrade.bootstrap(document.body, ['phonecatApp'], { strictDi: true });
+  }
+}
---
+++ src/ng2/app/ng2-demo/ng2-demo.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ng2-demo',
+  template: `
+    <h3>Angular 2 Demo Component</h3>
+    <img width="150" src="..." />
+  `
+})
+export class Ng2DemoComponent  {
+}
